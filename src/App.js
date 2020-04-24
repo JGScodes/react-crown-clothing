0366c456f8ffc8edd9e61785a17d43e4ecfe8d94
Jaime <jaimesof82@gmail.com>
2020-04-23 22:00:22 -0700

    Added the CollectionPreviw component and filtered and mapped shop data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch, Link } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import './App.css';
 
 const HatsPage = () => {
@@ -18,6 +19,7 @@ function App() {
           <Switch>
             <Route exact path="/" component={HomePage}/>
             <Route path="/hats" component={HatsPage}/>
+            <Route path="/shop" component={ShopPage}></Route>
           </Switch>
         </div>
   )
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import SHOP_DATA from './shop.data';
+import CollectionPreview from '../../components/collection-preview/collection-preview';
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA
+    }
+  }
+
+  render() {
+    // destructuring values from collections
+    const {collections} = this.state;
+    return (
+      <div className='shop-page'>
+          {
+            // destructuring the 'title' and 'items' from SHOP_DATA in otherCollectionProps and passing it to CollectionPreview component to be filtered and mapped over in the CollectionPreview component
+            collections.map(({id, ...otherCollectionProps}) => {
+              return <CollectionPreview key={id} {...otherCollectionProps}/>
+            })
+          }
+      </div>
+
+      )
+
+  }
+}
+
+
+
+
+
+export default ShopPage;
